3df2dbaca7a0922c8f808e2d9bedccab411b0631
JairHuerta <jair.17huerta@gmail.com>
2021-10-27 16:34:35 -0500

    intento 3343 de subir la api a heroku

--- index.js
+++ index.js
@@ -2,17 +2,21 @@
 
 var mongoose = require('mongoose'); //--> declaramos dependencia con mongoose
 var app = require('./app'); //--> configuración de Express
-var port = 3800; //--> puerto estático
+// var port = 3800; //--> puerto estático
+
+// mongodb+srv://jair:<password>@cluster0.jrtfu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+// mongodb://localhost:27017/curso_mean_social
 
 /* Conexión DataBase */
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/curso_mean_social') //--> definimos la conexión con la base de datos
+mongoose.connect('mongodb+srv://jair:contraseña@cluster0.jrtfu.mongodb.net/curso_mean_social?retryWrites=true&w=majority', {useNewUrlParser: true}) //--> definimos la conexión con la base de datos
 		.then(() => {  //--> conexión correcta
 			console.log("La conexión a la base de datos realizada conrrectamente!!!");
 
 			/* Crear Servidor */
-			app.listen(port, () => {
-				console.log("Servidor corriendo en el puerto " + String(port));
+			app.listen(process.env.PORT || 3800, () => {
+				console.log("Servidor corriendo en el puerto 3800");
 			});
 
 		})
